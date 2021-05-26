656dc81019e6c81c5e5f54ea6701f132f448e686
Imdad Sardharwalla <imdad.sardharwalla@gmail.com>
2021-05-26 12:48:34 +0100

    Change comments to docstrings and fix spacing

--- notify.py
+++ notify.py
@@ -4,24 +4,23 @@ from pathlib import Path
 import requests
 
 
-# Return timestamp in the following format: dd/mm/YY H:M:S
 def get_timestamp():
+    '''Return timestamp in the following format: dd/mm/YY H:M:S'''
     return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
 
 class TelegramNotify:
 
-
     def __init__(self):
+        '''Create and initialise internal class variables.'''
         self._tokens = {}
         self._IDs = {}
         self._ID = None
         self._token = None
 
-
-    # Load the list of bots/tokens and recipients/IDs from JSON files
     def load_pathways(
             self, bots_path='bots.json', recipients_path='recipients.json'):
+        '''Load the list of bots/tokens and recipients/IDs from JSON files.'''
 
         with Path(bots_path).open('r') as f:
             self._tokens = json.load(f)
@@ -29,15 +28,13 @@ class TelegramNotify:
         with Path(recipients_path).open('r') as f:
             self._IDs = json.load(f)
 
-
-    # Set the token and ID to be used by the send() function
     def set_pathway(self, bot_name, recipient_name):
+        '''Set the token and ID to be used by the send() function.'''
         self._token = self._tokens[bot_name]
         self._ID = self._IDs[recipient_name]
 
-
-    # Send a notification using the set pathway
     def send(self, text):
+        '''Send a notification using the set pathway.'''
 
         def print_line(_success, text, error_msg='Error not specified'):
             print(f'{get_timestamp()}: ', end='')
@@ -70,7 +67,6 @@ class TelegramNotify:
         print_line(
             r.status_code == 200, text, f'Status code was {r.status_code}')
 
-
-    # Print a line of information to stdout
     def info(self, text):
+        '''Print a line of information to stdout.'''
         print(f'{get_timestamp()}: Info: {text}')

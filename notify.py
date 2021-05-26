45f55ff53ce56d377e44fe698cfecd712d90ff16
Imdad Sardharwalla <imdad.sardharwalla@gmail.com>
2021-05-26 13:12:18 +0100

    Refactor class and add test_pathway() function

--- notify.py
+++ notify.py
@@ -18,6 +18,10 @@ class TelegramNotify:
         self._ID = None
         self._token = None
 
+    def print_info(self, text):
+        '''Print a line of information to stdout.'''
+        print(f'{get_timestamp()}: Info: {text}')
+
     def load_pathways(
             self, bots_path='bots.json', recipients_path='recipients.json'):
         '''Load the list of bots/tokens and recipients/IDs from JSON files.'''
@@ -28,45 +32,51 @@ class TelegramNotify:
         with Path(recipients_path).open('r') as f:
             self._IDs = json.load(f)
 
+    def print_notification_info(self, success, text,
+                                error_msg='Error not specified'):
+        '''Print an info line to stdout about the success of the notification. Returns success variable.'''
+        print(f'{get_timestamp()}: ', end='')
+        if success:
+            print('Notification sent: ', end='')
+        else:
+            print(f'Notification FAILED [{error_msg}]: ', end='')
+        print(f'"{text}"')
+
+        return success
+
     def set_pathway(self, bot_name, recipient_name):
         '''Set the token and ID to be used by the send() function.'''
         self._token = self._tokens[bot_name]
         self._ID = self._IDs[recipient_name]
 
     def send(self, text):
-        '''Send a notification using the set pathway.'''
-
-        def print_line(_success, text, error_msg='Error not specified'):
-            print(f'{get_timestamp()}: ', end='')
-
-            if _success:
-                print('Notification sent: ', end='')
-            else:
-                print(f'Notification FAILED [{error_msg}]: ', end='')
-
-            print(f'"{text}"')
+        '''Send a notification using the set pathway. Returns True/False on success/failure.'''
 
         # If no token has been set, print an error
         if self._token is None:
-            print_line(
-                False, text,
+            return self.print_notification_info(
+                False,
+                text,
                 'No bot token was provided. Did you call set_pathway()?')
-            return
 
         # If no ID has been set, print an error
         if self._ID is None:
-            print_line(
-                False, text,
+            return self.print_notification_info(
+                False,
+                text,
                 'No recipient ID was provided. Did you call set_pathway()?')
-            return
 
         url = f"https://api.telegram.org/bot{self._token}"
         params = {'chat_id': self._ID, 'text': text}
 
         r = requests.get(url + "/sendMessage", params=params)
-        print_line(
-            r.status_code == 200, text, f'Status code was {r.status_code}')
+        return self.print_notification_info(
+            r.status_code == 200,
+            text,
+            f'Status code was {r.status_code}')
 
-    def info(self, text):
-        '''Print a line of information to stdout.'''
-        print(f'{get_timestamp()}: Info: {text}')
+    def test_pathway(self):
+        '''Send a test notification.'''
+        self.print_info('Testing pathway...')
+        if not self.send('Pathway test notification.'):
+            raise RuntimeError('Pathway test failed.')

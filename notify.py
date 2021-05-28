a583a9d4721ff2b04918a6d0625eaa055d028531
Imdad Sardharwalla <imdad.sardharwalla@gmail.com>
2021-05-28 13:56:05 +0100

    Add base Notify class and new Sleeper class

--- notify.py
+++ notify.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import json
 from pathlib import Path
 import requests
+import time
 
 
 def get_timestamp():
@@ -9,22 +10,52 @@ def get_timestamp():
     return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
 
-class TelegramNotify:
+class Notify:
 
     def __init__(self, name: str):
-        '''Create and initialise internal class variables.'''
-        self._tokens = {}
-        self._IDs = {}
-        self._ID = None
-        self._token = None
         self._name = name
+
+    def get_notification_text(self, text: str) -> str:
+        return f'{self._name}: {text}'
 
     def message(self, text: str):
         '''Prints text to stdout.'''
         print(f'{get_timestamp()}: {text}')
 
+    def notification_message(self, success: bool, text: str,
+                             error_msg: str = 'Error not specified') -> bool:
+        '''Printa a message about the success of the notification. Returns the success variable.'''
+        if success:
+            message = 'Notification sent: '
+        else:
+            message = f'Notification FAILED [{error_msg}]: '
+        message += f'"{text}"'
+
+        self.message(message)
+        return success
+
+    def send(self, text: str) -> bool:
+        '''Placeholder for a subclass method.'''
+        raise NotImplementedError('Override this with a subclass method.')
+
+    def test_pathway(self):
+        '''Placeholder for a subclass method.'''
+        raise NotImplementedError('Override this with a subclass method.')
+
+
+class TelegramNotify(Notify):
+
+    def __init__(self, name: str):
+        '''Create and initialise internal class variables.'''
+        super().__init__(name)
+        self._tokens = {}
+        self._IDs = {}
+        self._ID = None
+        self._token = None
+
     def load_pathways(
-            self, bots_path='bots.json', recipients_path='recipients.json'):
+            self, bots_path='telegram-bots.json',
+            recipients_path='telegram-recipients.json'):
         '''Load the list of bots/tokens and recipients/IDs from JSON files.'''
 
         with Path(bots_path).open('r') as f:
@@ -33,16 +64,6 @@ class TelegramNotify:
         with Path(recipients_path).open('r') as f:
             self._IDs = json.load(f)
 
-    def notification_message(self, success: bool, text: str,
-                                error_msg: str = 'Error not specified') -> bool:
-        '''Printa a message about the success of the notification. Returns the success variable.'''
-        if success: message = 'Notification sent: '
-        else: message = f'Notification FAILED [{error_msg}]: '
-        message += f'"{text}"'
-
-        self.message(message)
-        return success
-
     def set_pathway(self, bot_name: str, recipient_name: str):
         '''Set the token and ID to be used by the send() function.'''
         self._token = self._tokens[bot_name]
@@ -50,7 +71,7 @@ class TelegramNotify:
 
     def send(self, text: str) -> bool:
         '''Send a notification using the set pathway. Returns True/False on success/failure.'''
-        notification_text = f'{self._name}: {text}'
+        notification_text = self.get_notification_text(text)
 
         # If no token has been set, print an error
         if self._token is None:
@@ -84,7 +105,8 @@ class TelegramNotify:
 
 class URLGet:
 
-    def __init__(self, url:str, notify: TelegramNotify, failure_limit:int=10):
+    def __init__(
+            self, url: str, notify: TelegramNotify, failure_limit: int = 10):
         '''Create and initialise internal class variables.'''
         self._url = url
         self._notify = notify
@@ -102,7 +124,9 @@ class URLGet:
             self._notify.message(f'Accessed URL: {self._url}')
 
             if self._failure_count > 0:
-                self._notify.send(f'Reconnected to the URL after {self._failure_count} failure(s): {self._url}')
+                self._notify.send(
+                    f'Reconnected to the URL after {self._failure_count} '
+                    f'failure(s): {self._url}')
                 self._failure_count = 0
         else:  # failure
             self._notify.message(f'Failed to access URL: {self._url}')
@@ -111,6 +135,69 @@ class URLGet:
             # Send a warning notification if we haven't been able to
             # connect to the URL multiple times
             if self._failure_count >= self._failure_limit:
-                self._notify.send(f'Unable to reach URL (tried {self._failure_count} time(s)): {self._url}')
+                self._notify.send(
+                    f'Unable to reach URL (tried {self._failure_count} '
+                    f'time(s)): {self._url}')
 
         return [success, r]
+
+
+class Sleeper:
+
+    def __init__(
+            self, num_units: float, unit: str = 'seconds',
+            skip_initial: bool = True, print_status: bool = False):
+
+        num_units_f = float(num_units)
+
+        unit_lc = unit.lower()
+        if unit_lc == 's':
+            unit_lc = 'seconds'
+        elif unit_lc == 'm':
+            unit_lc = 'minutes'
+        elif unit_lc == 'h':
+            unit_lc = 'hours'
+        elif unit_lc == 'd':
+            unit_lc = 'days'
+
+        if num_units_f < 0:
+            raise ValueError(f'"{num_units}" should be a non-negative value')
+
+        if unit_lc == 'seconds':
+            self._seconds = num_units_f
+        elif unit_lc == 'minutes':
+            self._seconds = num_units_f * 60
+        elif unit_lc == 'hours':
+            self._seconds = num_units_f * 60 * 60
+        elif unit_lc == 'days':
+            self._seconds = num_units_f * 60 * 60 * 24
+        else:
+            raise ValueError(
+                f'"{unit}" should be one of the following: "seconds", '
+                '"s", "minutes", "m", "hours", "h", "days", "d"')
+
+        self._skip_initial = skip_initial
+        self._initial = True
+
+        self._print_status = print_status
+        self._wait_str = f'{num_units_f} {unit_lc}'
+
+    def wait(self):
+        if self._initial and self._skip_initial:
+            self._initial = False
+            if self._print_status:
+                print('Sleeper: Skipping initial wait...')
+            return True
+
+        if self._print_status:
+            print(
+                f'Sleeper: Waiting for {self._wait_str}...',
+                end='',
+                flush=True)
+
+        time.sleep(self._seconds)
+
+        if self._print_status:
+            print('done.')
+
+        return True

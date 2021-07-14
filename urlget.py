3c5da3c8add0c3a9ee819f47341ba3cc3887cf25
Imdad Sardharwalla <imdad.sardharwalla@gmail.com>
2021-07-14 12:58:59 +0100

    Allow a payload to be attached to the URL get request

--- urlget.py
+++ urlget.py
@@ -5,22 +5,25 @@ import requests
 class URLGet:
 
     def __init__(
-            self, url: str, notify: TelegramNotify, failure_limit: int = 10):
+        self, url: str, payload: dict, notify: TelegramNotify,
+        failure_limit: int = 10):
         '''Create and initialise internal class variables.'''
         self._url = url
+        self._payload = payload
         self._notify = notify
         self._failure_limit = failure_limit
         self._failure_count = 0
 
     def get(self):
         '''Attempt to access a URL. Provide helpful messages and notifications if this isn't possible.'''
-        r = requests.get(self._url)
+        r = requests.get(self._url, params=self._payload)
         success = False
 
         # Check that we successfully connected to the URL
         if r.status_code == 200:  # success
             success = True
-            self._notify.message(f'Accessed URL: {self._url}')
+            self._notify.message(
+                f'Accessed URL: {self._url} (payload: {self._payload})')
 
             if self._failure_count > 0:
                 self._notify.send(
